41219fbcd5cceb2c482e63db2ef85e1e5a333226
elky <elkykim@gmail.com>
2018-06-03 17:55:13 +0900

    임지 추출 프레임 수정. (초당 1개, 초당 16장)

--- ffmpeg_convert.py
+++ ffmpeg_convert.py
@@ -8,4 +8,4 @@ files = os.listdir(os.getcwd())
 if __name__ == '__main__':
 	for i in files:
 		if i.endswith('.mp4'):
-			os.system("ffmpeg -i {} -r 1 -t 30 {}-%2d.jpeg".format(i, i))
+			os.system("ffmpeg -i {} -r 16 -t 30 {}-%2d.jpeg".format(i, i))
